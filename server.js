7c92ac63afa196e09cb17a2a6bf3635048f7a709
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-07-28 16:51:27 +0100

    Adding PUT for updating expenses

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const cors = require("cors");
-const expenses = require("./Expense.json");
+let expenses = require("./Expense.json");
 const app = express();
 
 app.use(express.json());
@@ -53,12 +53,64 @@ app.post(
   }
 );
 
+app.put(
+  "/updateExpense/:id",
+  [
+    body("title").trim().escape().notEmpty().withMessage("Title is required"),
+    body("amount")
+      .trim()
+      .escape()
+      .notEmpty()
+      .withMessage("Amount is required")
+      .isFloat({ min: 0.01 })
+      .withMessage("Amount must be a number greater than 0"),
+    body("date", "date Must not be empty")
+      .trim()
+      .escape()
+      .notEmpty()
+      .withMessage("Date is required")
+      .isISO8601()
+      .withMessage("Invalid date format!"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        data: null,
+        error: errors,
+        message: "Validation error!",
+      });
+    }
+
+    const expenseId = Number(req.params.id);
+    const expenseToUpdate = expenses.find(
+      (expense) => expense.id === expenseId
+    );
+
+    if (!expenseToUpdate) {
+      return res.status(404).json({
+        data: null,
+        message: "Expense not found",
+      });
+    }
+
+    expenses = expenses.map((expense) => {
+      if (expense.id === expenseId) {
+        return { ...expense, ...req.body };
+      }
+      return expense;
+    });
+
+    res.status(200).json({
+      data: expenses,
+      message: "Expense updated successfully!",
+    });
+  }
+);
+
 app.delete("/deleteExpense/:id", (req, res) => {
   const expenseId = Number(req.params.id);
-  console.log(expenseId);
   const expense = expenses.find((expense) => expense.id === expenseId);
-  console.log(expenses);
-
   if (!expense) {
     return res.status(404).json({
       data: null,
